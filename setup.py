cc84df18156bfb80472aca11a7851de7673cbfd0
epogrebnyak <e.pogrebnyak@gmail.com>
2019-01-21 22:22:29 +0300

    pip-installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='boo',
+      version='0.1',
+      description='Russian corporate reports 2012-2017',
+      url='http://github.com/ru-corporate/sandbox',
+      author='Evgeniy Pogrebnyak',
+      author_email='e.pogrebnyak@gmail.ru',
+      license='MIT',
+      packages=['boo'],
+      zip_safe=False)
