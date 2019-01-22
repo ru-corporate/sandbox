7a066bee38b466aeee2f1ebfb237bc1856525207
epogrebnyak <e.pogrebnyak@gmail.com>
2019-01-22 10:27:10 +0300

    more todos

---
+++ notebook/typical.py
@@ -0,0 +1,15 @@
+"""Показать связь между типовыми переменными в балансе."""
+
+import boo
+df = boo.read_dataframe(2012)
+
+# без крупнейших
+subset = df[df.cf != 0][df.ok1 != 67].sort_values('sales')[:-100]
+subset.plot(x='sales', y='cash_in_oper_sales', kind='scatter')
+
+# много малых компаний
+lim = [0, 0.2 *10 ** 7]
+subset.plot(x='sales', y='cash_in_oper_sales', kind='scatter', xlim=lim, ylim=lim)
+
+# TODO: 
+# abline()
