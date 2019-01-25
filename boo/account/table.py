ef10db5926739e9c3fc9d348759249d8e1692bbf
epogrebnyak <e.pogrebnyak@gmail.com>
2019-01-25 21:51:47 +0300

    temp

--- boo/account/table.py
+++ boo/account/table.py
@@ -2,35 +2,37 @@ from boo.account.names import account_name
 from boo.account.variables import balance, opu, cf_oper, cf_inv, cf_fin 
 
 
-def row_items(pairs):
-    return [(p[0], p[1], account_name(p[0])) for p in pairs]
-
-def row(lst):
-    return " ".join(["|", " | ".join(lst), "|"])
-
-def newlined(gen):
-    return "\n".join(map(row,gen))
-
 HEADER = ["Код отчетности", "Переменная", "Наименование показателя"]
 MIDROW = [":------------:", ":--------:", ":---------------------:"]
 
 
-def table(gen):
-    return newlined([HEADER]+[MIDROW]+row_items(gen))
+def row_items(pairs):
+    return [(p[0], p[1], account_name(p[0])) for p in pairs]
 
+def add_pipes(lst):
+    row = ["|", " | ".join(lst), "|"]
+    return " ".join(row)
+
+def table_body(gen):
+    return [add_pipes(x) for x in [HEADER] + [MIDROW] + row_items(gen)]
+
+def header(text, level):
+    return "\n%s %s" % ("#"*level, text)
+    
+def newlined(gen):
+    return "\n".join(gen)
+
+def table(gen, text, level):
+    return newlined([header(text, level)]+table_body(gen))
+
+# WONTFIX: | 4400 | cf | Сальдо денежных потоков за отчетный период |
+# Это в последнем разделе, но на само мделе, сальдо по трем предыдущим
 
 if __name__ == '__main__':    
-    H = "\n###"
-    H4 = "\n####"
-    print()
-    print(H, "Баланс")
-    print(table(balance))
-    print(H, "Отчет о финансовых результатах")
-    print(table(opu))
-    print(H, "Отчет о движении денежных средств")
-    print(H4, "Операционная деятельность")
-    print(table(cf_oper))
-    print(H4, "Инвестицонная деятельность")
-    print(table(cf_inv))
-    print(H4, "Финансовая деятельность")
-    print(table(cf_fin))
+    print(table(balance, "Баланс", 3))
+    print(table(opu, "Отчет о финансовых результатах", 3))    
+    print(header("Отчет о движении денежных средств", 3))
+    print(table(cf_oper, "Операционная деятельность", 4))
+    print(table(cf_inv, "Инвестицонная деятельность", 4))
+    print(table(cf_fin, "Финансовая деятельность", 4))
+    
--- notebook/pick.py
+++ notebook/pick.py
@@ -6,13 +6,31 @@ try:
 except NameError:
     df = read_dataframe(2017)
 
+COLUMNS_1 = ['title', 'ok1', 'region', 'inn']
+COLUMNS_RUB = ['ta', 'sales', 'cf', 'cf_oper']
+SHORT_NAMES_BY_INN = {
+    '2460066195': "РусГидро",
+    '4716016979': "ФСК ЕЭС",
+    '7702038150': "Московский метрополитен",
+    '7721632827': "Концерн Росэнергоатом",
+    '7706664260': "Атомэнергопром"
+}
+
+def report(df, divide_by, sort_key):
+    x = df[COLUMNS_1+COLUMNS_RUB] \
+          [is_operational(df)]  \
+          .set_index('inn') 
+    x = rename(x)
+    x.loc[:,COLUMNS_RUB] = x.loc[:,COLUMNS_RUB].divide(divide_by).round(1)
+    return x.sort_values(sort_key, ascending=False)
+
 
 def is_financial(df):
     return df.ok1.isin([64, 65, 66, 67])
 
 
-def operational(df):
-    return df[(df['cf'] != 0) & (df['ta'] > 0)]
+def is_operational(df):
+    return (df.cf != 0) & (df.ta > 0)
 
 
 def dequote(df):
@@ -22,17 +40,9 @@ def dequote(df):
     return df
 
 
-SHORT_NAMES_BY_INN = {
-    '2460066195': "РусГидро",
-    '4716016979': "ФСК ЕЭС",
-    '7702038150': "Московский метрополитен",
-    '7721632827': "Концерн Росэнергоатом",
-    '7706664260': "Атомэнергопром"
-}
 
-
-def by_title(df, includes_text, excludes_text=None):
-    mask = df.title.map(lambda s: includes_text.lower() in s.lower())
+def by_title(df, includes):
+    mask = df.title.map(lambda s: includes.lower() in s.lower())
     return df[mask]
 
 
@@ -42,3 +52,10 @@ def reverse(df):
 
 def nlargest(df, key, n):
     return df.sort_values([key], ascending=False).head(n)
+
+def rename(df, rename_dict=SHORT_NAMES_BY_INN):
+    """Rename some titles."""
+    ix = df.index.isin(list(rename_dict.keys()))
+    df.loc[ix,['title']] = df.loc[ix].index.map(lambda inn: rename_dict[inn])
+    return df
+
