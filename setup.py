fd3dca5133075ef91b6979c6fdb24b23422b9f66
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2019-02-02 13:31:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-# 0.2 changes folder storage to ~/.boo and exposes file location helpers 
-#     raw_filepath(year) and processed_filepath(year)
+# 0.0.2 changes folder storage to ~/.boo and exposes file location helpers 
+#       raw_filepath(year) and processed_filepath(year)
 
 setup(name='boo',
       version='0.0.2',
